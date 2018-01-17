df833c89c95f06a9991374e6dbcf9e8e06859414
Michel van der Heide <michelvanderheide@gmail.com>
2018-01-17 16:17:22 +0100

    auto create mp4

--- stream/index.php
+++ stream/index.php
@@ -10,10 +10,20 @@ $imagedir = realpath(__DIR__.'/../images/');
 $videodir = realpath(__DIR__.'/../videos/');
 
 if ($_GET['create']) {
-
 	$videos = dirToArray($videodir);
 	foreach($videos as $video) {
 		$image = $imagedir.'/'.basename($video, '.mp4').'-1.png';
+
+      $pathinfo = pathinfo($video);
+      if ($pathinfo['extension'] == 'mov') {
+
+         $mp4video = $videodir.'/'.basename($video, '.mov').".mp4";
+         if (!file_exists($mp4video)) {
+            $cmd = "ffmpeg -i ".$videodir.'/'.$video." -vcodec -strict -c  copy ".$mp4video;
+            shell_exec($cmd);
+         }
+      }
+      
 		if (!file_exists($image)) {
 			$videofile = $videodir.'/'.$video;
 			$cmd = "ffmpeg -i $videofile -r 1 -f image2 $image";
@@ -26,11 +36,12 @@ if ($_GET['create']) {
 } else if ($_GET['id']) {
    $type = 'mp4';
 	$filepath = $videodir.'/'.$_GET['id'].'.mp4';
-   $stream = new VideoStream($filepath);
+
    if (!file_exists($filepath)) {
       $filepath = $videodir.'/'.$_GET['id'].'.mov';
       $type = 'mov';
    }
+   $stream = new VideoStream($filepath);
 	$stream -> start($type);
    exit($filepath);
 	//exit($filepath);
